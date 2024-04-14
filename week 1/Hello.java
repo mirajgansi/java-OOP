db9b896953be016ee337fff2c0692735d272352b
mirajgansi <mirajgasi122@gmail.com>
2024-04-14 12:17:30 +0545

    week 1 of comment

--- week 1/Hello.java
+++ week 1/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        //1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+        System.out.println("hello world!!");
     }
 }

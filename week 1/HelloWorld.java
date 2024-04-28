375b2b978f02e33aa68a772c50c27862279a835d
mirajgansi <mirajgasi122@gmail.com>
2024-04-28 12:54:54 +0545

    hh

---
+++ week 1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void  main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("New Line");
+        System.out.print("this \t");
+        System.out.print("is \t");
+        System.out.print("\"java\"");  
+    }
+}
---
+++ week 1/Task1.java
@@ -0,0 +1,30 @@
+//print the follownf system.ouyptiny()
+// public class Task{
+//     public static void main(String[] args){
+
+//     System.out.print("this is \n");
+//     System.out.print("My first's\n");
+//     System.out.print("\"java programming\"code");
+//     }
+// }
+
+// public class Task{
+//     public static void main(String[] args){
+//         System.out.print("we're albe \n");
+//         System.out.print(" To\\do\\ This \n");
+//         System.out.print("\'In\' a \"single\" Statemenr");
+//     }
+// }
+
+// task 3
+
+// public class Task{
+//     public static void main(String[] args) {
+//         System.out.println("\"*\"");
+//         System.out.println("\'**\'");
+//         System.out.println("\\***\\");
+//         System.out.println("*** ***");
+//         System.out.println("\"*\"\"*\"\"*\"");
+
+//     }
+// }
